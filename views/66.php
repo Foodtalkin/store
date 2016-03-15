5a5e5f8aff2e49f5cdbb1a5824fb9e9bbefde85c
rchshrm16 <rchshrm16@gmail.com>
2016-03-15 13:09:28 +0530

    changes in contest form

--- views/66.php
+++ views/66.php
@@ -227,7 +227,7 @@
               if(data[4]['value'] != ''){
                 var dob = moment(data[4]['value']).format("YYYY-MM-DD");
               }else{
-                var dob = <?php echo $user['dob']; ?>;
+                var dob = (<?php echo $user['dob']; ?> != null)?<?php echo $user['dob']; ?>: "0000-00-00";
               }
               // var meta = <?php echo json_encode($user['metadata']); ?>;
               // //console.log(meta);
