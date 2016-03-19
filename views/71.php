a9431fb24b50e726993c385b3579150ced1da47b
rchshrm16 <rchshrm16@gmail.com>
2016-03-19 13:07:12 +0530

    csv for users

--- views/71.php
+++ views/71.php
@@ -218,7 +218,7 @@
             //Serialize the form data
               //console.log("validatetrue");
               
-              
+              var data = $('#frm').serializeArray();
               if(data[0]['value'] != ''){
                 var name = data[0]['value'];
               }else{
