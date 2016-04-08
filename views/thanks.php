6f16a183386e02785a097dc1ac59bb7bb8ce5e39
rchshrm16 <rchshrm16@gmail.com>
2016-04-08 17:56:43 +0530

    new Event

--- views/thanks.php
+++ views/thanks.php
@@ -40,9 +40,11 @@ session_start();
         $msg = "Your name is on 'The List' <br>
                 Please Note: Club Rules Apply**";
       } elseif ($_SESSION['type'] == "contest") {
-        $icon = "../img/goodluck.png";
-        $msg = "Now that was a piece of cake right? <br>
+        $icon = "../img/thankyou.png";
+        $icon1 = "../img/goodluck.png";
+        $msg1 = "Now that was a piece of cake right? <br>
                 Stay tuned to know if you made it to 'The List'";
+                $msg="";
       } else{
         $icon = "../img/thankyou.png";
         $msg1 = "You just booked yourself a Food Talk Event <br>
