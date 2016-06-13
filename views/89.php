8e26d4862204e88035a8261b17497d8f4176d78c
rchshrm16 <rchshrm16@gmail.com>
2016-06-13 16:56:01 +0530

    contest 88

--- views/89.php
+++ views/89.php
@@ -158,13 +158,13 @@
 
     <body>
       <div class="intro hide-on-med-and-up">
-        <img src="../img/cover88.jpg" class="responsive-img">
+        <img src="../img/cover89.jpg" class="responsive-img">
       </div>
       <div class="container">
         <div class="row hide-on-small-only">
           <div class="col s12 m12 l12">
             <div class="intro">
-              <img src="../img/cover88.jpg" class="responsive-img">
+              <img src="../img/cover89.jpg" class="responsive-img">
             </div>
           </div>
         </div>
