b4e62a45dda6880d5a0311f2c31ff878700d1ae5
rchshrm16 <rchshrm16@gmail.com>
2016-06-14 11:47:14 +0530

    thanks page

--- views/thanks.php
+++ views/thanks.php
@@ -219,7 +219,8 @@ session_start();
 
   </html>
   <?php 
-    if($_SESSION['target'] == "88"){
+    echo $_SESSION['target'];
+    if($_SESSION['target'] == 88){
       header('Location: https://www.iorderfresh.com/');
     }
    $_SESSION['source'] = null;
