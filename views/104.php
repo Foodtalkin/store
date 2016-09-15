5cbb32bddf119446cc6f0833fd26ba046a8a0e5e
shalga <shalga@foodtalkindia.com>
2016-09-15 10:59:57 +0530

    104 event added

--- views/104.php
+++ views/104.php
@@ -146,15 +146,15 @@
              
                 <h4 class="brand-heading">Food Talk India Presents #WWIM14- in partnership with Instagram</h4>
                 <h5>AntiSocial, HKV | Time: 8PM- 10PM | Designed by Rani Pink</h5>
-                <h5>It's time for the official #WWIM14 and this year's it's all about Food  - in case you haven't been following their awesome blog it's the "World Wide Insta Meet No.14"!!</h5>
+                <h5>It's time for the official #WWIM14 and this year it's all about Food  - in case you haven't been following their awesome blog, it's the "World Wide Insta Meet No.14"!!</h5>
                 <h5>What goes best with Food? Cocktails!</h5>
                 <h4>Food Talk India presents - The Cocktail Workshop</h4>
-                <h5>Time to Meet and Greet with a tipple and a tale!</h5>
+                <h5>Time to Meet and Greet with a tipple & a tale!</h5>
                 <div class="borderbox">
-                  <h4 class="center">Sign up and stand a chance to get invited! Be part of the Tastiest day of the year</h4>
+                  <h4 class="center">Sign up & stand a chance to get invited! Be part of the Tastiest day of the year</h4>
                 </div>
                 
-                <h5>This isn't your regular Cocktail workshop! The theme is ABC - Anything But Cups. That's right folks bring your own drinking vessel - be it a shoe, boot, vase or ever a flower cup that you can drink out of. Learn to whip up delicious cocktails and drink in an unconventional way!</h5>
+                <h5>This isn't your regular Cocktail workshop!</h5><h5>The theme is ABC - Anything But Cups. That's right folks! Bring your own drinking vessel - be it a shoe, boot, vase or ever a flower cup that you can drink out of. Learn to whip up delicious cocktails and drink in an unconventional way!</h5>
                 
                 <!--  <h5>Sunday, 31st July | 6PM Onwards | Perch, Vasant Vihar</h5> -->
                  
