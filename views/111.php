4b072f508749c8c9c8a828b794a09663ea55cdbe
rchshrm16 <rchshrm16@gmail.com>
2016-12-09 13:09:15 +0530

    111

--- views/111.php
+++ views/111.php
@@ -319,20 +319,22 @@
                 
                 // // console.log(meta);
                  var meta = <?php echo json_encode($user['metadata']); ?>;
-                if(meta != null){
-                  meta['Age']= $('#Question2').val();
-                }else{
-                  
-                  // if($('#Question2').val() != ''){
-                  //   meta['Age']= $('#Question2').val();
-                  //   // meta['VegNonVeg']= $('#vegnvg option:selected').val();
-                  // }
+                 console.log(meta);
+                if(meta == null){
                   var meta = {
                     Age: $('#Question2').val()
                     
                     // VegNonVeg : $('#vegnvg option:selected').val()
                   }
+                  meta['Age']= $('#Question2').val();
+                }else{
+                  // meta['Age']= $('#Question2').val();
+                  if($('#Question2').val() != ''){
+                    meta['Age']= $('#Question2').val();
+                    // meta['VegNonVeg']= $('#vegnvg option:selected').val();
+                  }
                 }
+                console.log(meta);
                 // console.log($('#vegnvg option:selected').val());
                 // var response = {
                 //   guest : $('#Question1').val()
